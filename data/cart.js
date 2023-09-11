d0f939a421ddb60d4878f17049bdec52d846a268
akhilbju <abiju60@gmail.com>
2023-09-12 00:42:56 +0530

    fixed radio selector

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,10 @@
 export const cart = [{
     productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quatity : 2
+},
+{
+    productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quatity : 2
 }
 ]; 
 
