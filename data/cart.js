df7fc773bc15eccdc235aa5243afca49f9fccd14
akhilbju <abiju60@gmail.com>
2023-09-12 10:55:57 +0530

    add to cart

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,18 @@
-export let cart = [{
-    productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quatity : 2
-}, 
-{
-    productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quatity : 1
+export let cart = JSON.parse(localStorage.getItem('cart'));
+  
+
+if(!cart){
+    cart = [{
+        productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quatity : 2
+    }
+    ]; 
 }
-]; 
+
+
+function saveToStorage(){
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId){
     let matchingItem;
@@ -24,9 +30,10 @@ export function addToCart(productId){
      cart.push({
          productId : productId,
          quatity : 1
-     })
+     });
   
     } 
+    saveToStorage();
   }
   export function deleteFromCart(productId){
     const newCart =[];
@@ -36,7 +43,8 @@ export function addToCart(productId){
         } 
 
     });
-    cart = newCart;
+    cart = newCart; 
+    saveToStorage() ;
     
 
   }
